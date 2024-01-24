5da1e734e6cd825756f341f589bf5764c2fa0d6f
lauramediedk <lauramediedk@gmail.com>
2024-01-24 15:54:34 +0000

    neo4j and flask connection

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
 from flask import Flask
+import json
+from neo4j import GraphDatabase
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+#neo4j
+NEO4J_URI = 'neo4j+s://8bc886ed.databases.neo4j.io'
+NEO4J_USER = 'neo4j'
+NEO4J_PASSWORD = '4Ka5DqTW0WC9-ZbxMV0x9ZdCiagHCRxHDkYvZc6GSFw'
+
+# Driver instantiation
+driver = GraphDatabase.driver(
+    NEO4J_URI,
+    auth=(NEO4J_USER, NEO4J_PASSWORD)
+)
+
+def run_query(query):
+    with driver.session() as session:
+        result = session.run(query)
+        return result.data()
+
+@app.route("/")
+def check_database():
+    # Your Cypher query
+    cypher_query = "MATCH (p:Person) RETURN p LIMIT 4"
+
+    try:
+        result = run_query(cypher_query)
+        return json.dumps(result), 200  # Use json.dumps() to convert to JSON
+    except Exception as e:
+        return f"Error: {str(e)}", 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
