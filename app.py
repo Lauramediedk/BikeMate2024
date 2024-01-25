cf1906ffc7b723d91f4b3b74296630944ac809e2
lauramediedk <lauramediedk@gmail.com>
2024-01-25 10:48:34 +0000

    cleanup

--- app.py
+++ app.py
@@ -1,19 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask
 from config import SECRET_KEY
 from db import db
 
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
-
-@app.route("/")
-def check_database():
-    query = "MATCH (p:person) RETURN p LIMIT 4"
-
-    try:
-        result = db.run_query(query)
-        return jsonify(result), 200
-    except Exception as e:
-        return f"Error: {str(e)}", 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
