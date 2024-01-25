58cab9c17549042f859128f0d9eb6552d77bc956
lauramediedk <lauramediedk@gmail.com>
2024-01-25 14:34:39 +0000

    too many blank lines

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
 from neo4j import GraphDatabase
 from config import NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD
 
+
 class Database:
     def __init__(self, NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD):
         self._driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
-
 
     def close(self):
         self._driver.close()
