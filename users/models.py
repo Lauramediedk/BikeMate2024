4fbf42cfd754a5741a2e83993373e15e6c0d6bd8
lauramediedk <lauramediedk@gmail.com>
2024-01-25 21:48:33 +0000

    basic signup setup but needs fix

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from db import db
+
+class User:
+    def __init__(self, first_name, last_name, email, hashed_password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.hashed_password = hashed_password
+
+    def signup_user(self):
+        query = (
+            "CREATE (user:User {first_name: $first_name, last_name: $last_name, email: $email, hashed_password: $password})"
+        )
+        parameters = {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "password": self.hashed_password,
+        }
+
+        try:
+            db.run_query(query, parameters)
+        except Exception as e:
+            raise RuntimeError(f"Error registrering user: {str(e)}")
--- users/views.py
+++ users/views.py
@@ -1,8 +1,24 @@
-from flask import render_template
+from flask import render_template, flash
 from . import users_bp
 from .forms import SignupForm
+from .models import User
+from werkzeug.security import generate_password_hash
 
-@users_bp.route('/signup')
+@users_bp.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
+    if form.validate_on_submit():
+        try:
+            user = User(
+                first_name = form.first_name.data,
+                last_name = form.last_name.data,
+                password = form.password.data,
+                hashed_password = generate_password_hash(form.password.data)
+            )
+            user.signup_user()
+
+            flash('Bruger oprettet', 'success')
+        except RuntimeError as e:
+            flash(f"Fejl ved oprettelsen: {str(e)}")
+
     return render_template('signup.html', form=form)
