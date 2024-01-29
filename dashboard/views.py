65ab3a3897d037978157ba0cb3c8c92a351460e1
lauramediedk <lauramediedk@gmail.com>
2024-01-29 12:49:55 +0000

    check if user is logged in and save their first name in the session with custom login_required def

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,13 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, session
 from . import dashboard_bp
+from users.views import login_required
 
 @dashboard_bp.route('/dashboard')
+@login_required
 def dashboard():
-    return render_template('dashboard.html')
+    if 'first_name' in session:
+        first_name = session['first_name']
+        return render_template('dashboard.html', first_name=first_name)
+    else:
+        flash('Adgang nægtet', 'error')
+    return redirect(url_for('users.login'))
--- users/__init__.py
+++ users/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 users_bp = Blueprint('users', __name__)
 
-from .views import signup, login  # noqa
+from .views import signup, login, login_required # noqa
