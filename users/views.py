81a16fa9f29b9f1deb295c3cec3aa6f873e32072
lauramediedk <lauramediedk@gmail.com>
2024-01-29 07:41:06 +0000

    login and quick dashboard setup to test it

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
-from flask import render_template, flash
+from flask import render_template, flash, redirect, url_for
 from werkzeug.security import generate_password_hash
 from . import users_bp
-from .forms import SignupForm
+from .forms import SignupForm, LoginForm
 from .models import User
 
 
@@ -23,3 +23,23 @@ def signup():
             flash(f"Fejl ved oprettelsen: {str(e)}")
 
     return render_template('signup.html', form=form)
+
+
+@users_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        try:
+            user = User.find_user_email(form.email.data)
+            if user and user.check_password(form.password.data):
+                flash('Log ind godkendt', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                flash('Log ind mislykkedes', 'error')
+
+        except RuntimeError as e:
+            flash(f"Fejl ved log ind: {str(e)}")
+    return render_template('login.html', form=form)
+
+def dashboard():
+    return render_template('dashboard.html')
