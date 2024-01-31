c236ff95ac445f6ab0e5a089c88435c6c26d67a8
lauramediedk <lauramediedk@gmail.com>
2024-01-31 13:17:38 +0000

    navbar different for logged in users and not

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from config import SECRET_KEY
 from users import users_bp
 from dashboard import dashboard_bp
@@ -10,6 +10,10 @@ app.secret_key = SECRET_KEY
 app.register_blueprint(users_bp, url_prefix='/users')
 app.register_blueprint(dashboard_bp, url_prefix='/dashboard')
 app.register_blueprint(challenges_bp, url_prefix='/challenges')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
