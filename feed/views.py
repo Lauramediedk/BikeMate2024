87fb797942fdd5ac06aca93ea399260e5d81229e
lauramediedk <lauramediedk@gmail.com>
2024-02-01 12:24:12 +0000

    basic feed structure and toggle element

--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,8 @@
 from . import feed_bp
 from flask import render_template
+from users.views import login_required
 
 @feed_bp.route('/')
+@login_required
 def feed():
-    return render_template('feed.html')
+    return render_template('feed.html', active_page='feed')
