b6c7b42e98e5e4f0a9b9d9306fd0e7ca6256ba0d
lauramediedk <lauramediedk@gmail.com>
2024-03-12 12:12:37 +0000

    users can now add friends and if they lookup the friends then it will be visible that they are friends and they can unfriend them again

--- friends/models.py
+++ friends/models.py
@@ -1,5 +1,45 @@
 from db import db
 
+def check_friendship(from_user_id, to_user_id):
+    query = (
+        """
+        MATCH (fromUser:User {user_id: $from_user_id})-[r:FRIENDS_WITH]-(toUser:User {user_id: $to_user_id})
+        RETURN EXISTS((fromUser)-[r]-()) AS isFriend
+        """
+    )
+
+    parameters = {
+        "from_user_id": from_user_id,
+        "to_user_id": to_user_id,
+    }
+
+    try:
+        result = db.run_query(query, parameters)
+        return result[0]['isFriend'] if result else False
+    except Exception as e:
+        raise RuntimeError(f"Kunne ikke tjekke venskabsstatus: {str(e)}") from e
+
+def delete_friendship(from_user_id, to_user_id):
+    query = (
+        """
+        MATCH (fromUser:User)-[r:FRIENDS_WITH]-(toUser:User)
+        WHERE fromUser.user_id = $from_user_id AND toUser.user_id = $to_user_id
+        DELETE r
+        RETURN fromUser, toUser
+        """
+    )
+
+    parameters = {
+        "from_user_id": from_user_id,
+        "to_user_id": to_user_id,
+    }
+
+    try:
+        db.run_query(query, parameters)
+        return True
+    except Exception as e:
+        raise RuntimeError(f"Kunne ikke slette anmodningen: {str(e)}") from e
+
 def send_friend_request(from_user_id, to_user_id):
     query = (
         """
@@ -20,13 +60,33 @@ def send_friend_request(from_user_id, to_user_id):
     except Exception as e:
         raise RuntimeError(f"Kunne ikke afsende anmodning: {str(e)}") from e
 
+def get_friend_requests(to_user_id):
+    query = (
+        """
+        MATCH (fromUser:User)-[r:FRIEND_REQUEST]->(toUser:User {user_id: $to_user_id})
+        RETURN fromUser.user_id AS fromUserId, fromUser.first_name AS firstName,
+        fromUser.last_name AS lastName,
+        fromUser.image_path AS imagePath
+        """
+    )
+
+    parameters = {
+        "to_user_id": to_user_id,
+    }
+
+    try:
+        result = db.run_query(query, parameters)
+        return result
+    except Exception as e:
+        raise RuntimeError(f"Kunne ikke henter anmodninger: {str(e)}") from e
+
 def accept_friend_request(from_user_id, to_user_id):
     query = (
         """
         MATCH (fromUser:User)-[r:FRIEND_REQUEST]->(toUser:User)
         WHERE fromUser.user_id = $from_user_id AND toUser.user_id = $to_user_id
         DELETE r
-        CREATE (fromUser)-[:FRIENDS_WITH]->(toUser)
+        MERGE (fromUser)-[:FRIENDS_WITH]-(toUser)
         RETURN fromUser, toUser
         """
     )
@@ -42,19 +102,51 @@ def accept_friend_request(from_user_id, to_user_id):
     except Exception as e:
         raise RuntimeError(f"Kunne ikke afsende anmodning: {str(e)}") from e
 
-def search_user(search_term):
+def delete_friend_request(from_user_id, to_user_id):
+    query = (
+        """
+        MATCH (fromUser:User)-[r:FRIEND_REQUEST]->(toUser:User)
+        WHERE fromUser.user_id = $from_user_id
+        DELETE r
+        RETURN fromUser, toUser
+        """
+    )
+
+    parameters = {
+        "from_user_id": from_user_id,
+        "to_user_id": to_user_id,
+    }
+
+    try:
+        db.run_query(query, parameters)
+        return True
+    except Exception as e:
+        raise RuntimeError(f"Kunne ikke slette anmodningen: {str(e)}") from e
+
+def search_user(search_term, user_id):
     query = (
         """
         CALL db.index.fulltext.queryNodes("firstname_and_lastname", $search_term)
         YIELD node, score
-        RETURN node.first_name AS firstName, node.last_name AS lastName, node.image_path AS imagePath, score
+        WHERE NOT node.user_id = $user_id
+        OPTIONAL MATCH (:User {user_id: $user_id})-[r:FRIEND_REQUEST]->(node)
+        OPTIONAL MATCH (:User)-[f:FRIENDS_WITH]-(node)
+        RETURN
+            node.user_id AS userId,
+            node.first_name AS firstName,
+            node.last_name AS lastName,
+            node.image_path AS imagePath,
+            score,
+            CASE WHEN r IS NOT NULL THEN true ELSE false END AS requestSent,
+            CASE WHEN f IS NOT NULL THEN true ELSE false END AS isFriend
         ORDER BY score DESC
         """
     )
     # Get the most relevant/closer match of results by using score
 
     parameters = {
-        "search_term": search_term
+        "search_term": search_term,
+        "user_id": user_id,
     }
 
     try:
--- friends/views.py
+++ friends/views.py
@@ -1,31 +1,63 @@
 from flask import render_template, session, flash, url_for, redirect, request
 from . import friends_bp
 from users.views import login_required
-from .models import send_friend_request, search_user
+from .models import send_friend_request, search_user, get_friend_requests, accept_friend_request, delete_friend_request, check_friendship, delete_friendship
 
 @friends_bp.route('/')
 @login_required
 def friends():
     search_term = request.args.get('search_term', '')
+    user_id = session.get('user_id')
+    friend_requests = get_friend_requests(user_id)
     results = []
 
     if search_term:
-        results = search_user(search_term)
+        results = search_user(search_term, user_id)
         if not results:
             flash('Kunne ikke finde nogle brugere med det navn', 'error')
 
     return render_template('friends.html',
                            search_term=search_term,
+                           friend_requests=friend_requests,
                            results=results,
                            active_page='friends')
 
-@friends_bp.route('/send_request/<to_user_id>', methods=['POST'])
+@friends_bp.route('/accept_request/<from_user_id>', methods=['POST'])
 @login_required
-def send_request(to_user_id):
+def accept_request(from_user_id):
+    to_user_id = session.get('user_id')
+
+    if accept_friend_request(from_user_id, to_user_id):
+        flash('Tilføjet til venner', 'success')
+    else:
+        flash('Kunne ikke tilføje bruger som ven, prøv igen', 'error')
+    return redirect(url_for('friends.friends'))
+
+@friends_bp.route('/delete_request/<from_user_id>', methods=['POST'])
+@login_required
+def delete_request(from_user_id):
+    to_user_id = session.get('user_id')
+
+    if delete_friend_request(from_user_id, to_user_id):
+        flash('Venne anmodning slettet', 'success')
+    else:
+        flash('Anmodningen kunne ikke slettes, prøv igen', 'error')
+    return redirect(url_for('friends.friends'))
+
+@friends_bp.route('/toggle_friendship/<to_user_id>', methods=['POST'])
+@login_required
+def toggle_friendship(to_user_id):
     from_user_id = session.get('user_id')
 
-    if send_friend_request(from_user_id, to_user_id):
-        flash('Anmodning sendt til brugeren', 'success')
+    if check_friendship(from_user_id, to_user_id):
+        if delete_friendship(from_user_id, to_user_id):
+            flash('Fjernet fra venner', 'success')
+        else:
+            flash('Kunne ikke fjerne fra venner, prøv igen', 'error')
     else:
-        flash('Anmodningen kunne ikke sendes, prøv igen', 'error')
+        if send_friend_request(from_user_id, to_user_id):
+            flash('Anmodning sendr til brugeren', 'success')
+        else:
+            flash('Anmodningen kunne ikke sendes, prøv igen', 'error')
+
     return redirect(url_for('friends.friends'))
