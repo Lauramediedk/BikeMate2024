8b64bb01d5ca64e3f7a1f1fc37f04a951ec08617
lauramediedk <lauramediedk@gmail.com>
2024-03-21 18:32:47 +0000

    linting warnings fix

--- notifications/views.py
+++ notifications/views.py
@@ -26,7 +26,6 @@ def update_notifications():
     user_id = session.get('user_id')
     invitations = get_user_invites(user_id)
     friend_requests = get_friend_requests(user_id)
-    new_notifications = len(invitations) > 0 # Check if there is any
-    session['new_notifications'] = new_notifications # Save it in session to use all places
-    # Render only the notification list part of the template
+    new_notifications = len(invitations) > 0  # Check if there is any
+    session['new_notifications'] = new_notifications  # Save it in session to use all places
     return render_template('notif_list.html', invitations=invitations, friend_requests=friend_requests)
